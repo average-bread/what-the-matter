7dde0fa0b42c45bc6dee3adc5234f838be6c9d47
average-bread <163648686+average-bread@users.noreply.github.com>
2024-03-31 00:01:55 +0100

    Update

--- src/main/java/cursedbread/whatsthematter/matterblocks/SpawnMatter.java
+++ src/main/java/cursedbread/whatsthematter/matterblocks/SpawnMatter.java
@@ -3,7 +3,11 @@ package cursedbread.whatsthematter.matterblocks;
 import cursedbread.whatsthematter.WhatsTheMatter;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.player.gamemode.Gamemode;
 import net.minecraft.core.world.World;
+
+import java.util.Random;
 
 public class SpawnMatter extends Block {
 	public SpawnMatter(String key, int id) {
@@ -11,13 +15,28 @@ public class SpawnMatter extends Block {
 	}
 	int i;
 	int j;
+	int k;
+	int q;
+	int t;
+	int o;
 
-	public void onBlockDestroyedByExplosion(World world, int x, int y, int z) {
-		for (i = -1; i <= 1; i++) {
-			for (j = -1; j <= 1; j++) {
-				if (world.getBlockId(x + i, y, z +j) == 0) {
-					world.setBlock(x + i, y, z +j, WhatsTheMatter.whiteMatter.id);
-				}
+	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
+		for (i= -1; i<=1; i++) {
+			if (world.getBlockId(x, y+i, z) !=0) {
+				q = world.getBlockId(x, y+i, z);
+				world.setBlock(x, y-(i+i), z, q);
+			}
+		}
+		for (j= -1; j<=1; j++) {
+			if (world.getBlockId(x+j, y, z) !=0) {
+				t = world.getBlockId(x + j, y, z);
+				world.setBlock(x - (j+j), y, z, t);
+			}
+		}
+		for (k= -1; k<=1; k++) {
+			if (world.getBlockId(x, y, z+k) !=0) {
+				o = world.getBlockId(x, y, z+k);
+				world.setBlock(x, y, z-(k+k), o);
 			}
 		}
 	}
